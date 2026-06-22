9a04c78a02f3ccbea540b8d1b100f2644a8d7e04
rezatajari <reza.tajari70@gmail.com>
2026-06-22 12:34:35 +0330

    feat: add Book model to API and reference API project from Client

---
+++ LibraryAppMVC.Shared/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAppMVC.Shared.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public int YearPublished { get; set; }
+}
