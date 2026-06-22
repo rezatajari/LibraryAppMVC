8d250358012ee6151adc69269432aa7cf8a441c8
rezatajari <reza.tajari70@gmail.com>
2026-06-22 12:37:36 +0330

    feat: add BooksController with in-memory static book list

---
+++ LibraryAppMVC.API/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibraryAppMVC.Shared.Models;
+
+namespace LibraryAppMVC.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private static readonly List<Book> _books = new()
+        {
+            new Book { Id = 1, Title = "شاهنامه", Author = "فردوسی", YearPublished = 1010 },
+            new Book { Id = 2, Title = "بوف کور", Author = "صادق هدایت", YearPublished = 1937 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
+    }
+}
