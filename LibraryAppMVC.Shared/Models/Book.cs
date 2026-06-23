df21ccb6a967731f41acd82aa6dece0981d8e43e
rezatajari <reza.tajari70@gmail.com>
2026-06-23 17:02:47 +0330

    feat(shared): add IsAvailable property to Book entity

--- LibraryAppMVC.Shared/Models/Book.cs
+++ LibraryAppMVC.Shared/Models/Book.cs
@@ -6,4 +6,5 @@ public class Book
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
     public int YearPublished { get; set; }
+    public bool IsAvailable { get; set; } = true;
 }
---
+++ LibraryAppMVC.Shared/Models/BorrowRecord.cs
@@ -0,0 +1,5 @@
+﻿namespace LibraryAppMVC.Shared.Models;
+
+public class BorrowRecord
+{
+}
