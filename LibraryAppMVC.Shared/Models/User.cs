69d8fc0afe61dbb82e27b31329bde61f25fc52e3
rezatajari <reza.tajari70@gmail.com>
2026-06-23 19:04:37 +0330

    refactor(layout): invoke StateHasChanged in OnParametersSet to force menu redraw

--- LibraryAppMVC.Shared/Models/User.cs
+++ LibraryAppMVC.Shared/Models/User.cs
@@ -1,21 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace LibraryAppMVC.Shared.Models;
 
 public class User
 {
     public int Id { get; set; }
-
-    [Required(ErrorMessage = "Full Name is required.")]
-    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
     public string FullName { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "Email is required.")]
-    [EmailAddress(ErrorMessage = "Invalid Email Address.")]
     public string Email { get; set; } = string.Empty;
-
     public string? PhoneNumber { get; set; }
-    [Required]
     public string Password { get; set; } = string.Empty;
     public DateTime MembershipDate { get; set; } = DateTime.Now;
 }
