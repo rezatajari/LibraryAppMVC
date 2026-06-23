3616742d7edff04c4d00ff1259c4ca4887ce247b
rezatajari <reza.tajari70@gmail.com>
2026-06-23 17:53:17 +0330

    feat(api): add UsersController with GET and POST endpoints

---
+++ LibraryAppMVC.API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryAppMVC.API.Data;
+using LibraryAppMVC.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAppMVC.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController(LibraryDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(User user)
+    {
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+    }
+}
