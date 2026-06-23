08171b8999c43ee6222ec6ba55d1d0482f535e1d
rezatajari <reza.tajari70@gmail.com>
2026-06-23 17:06:42 +0330

    feat: BorrowController

---
+++ LibraryAppMVC.API/Controllers/BorrowController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAppMVC.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BorrowController : ControllerBase
+{
+}
---
+++ LibraryAppMVC.API/Migrations/20260623133520_borrowrecord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryAppMVC.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class borrowrecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "BorrowRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    BorrowerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BorrowDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BorrowRecords_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowRecords_BookId",
+                table: "BorrowRecords",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BorrowRecords");
+
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Books");
+        }
+    }
+}
