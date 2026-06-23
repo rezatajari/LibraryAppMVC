b058570987fa0c391955b948f966944156763bea
rezatajari <reza.tajari70@gmail.com>
2026-06-23 13:44:29 +0330

    feat: UserRole into entities

--- LibraryAppMVC.Client/src/app/app.config.ts
+++ LibraryAppMVC.Client/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from "./app.routes";
 
 import { routes } from './app.routes';
 
