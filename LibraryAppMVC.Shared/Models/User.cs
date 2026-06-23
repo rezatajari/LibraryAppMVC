961dfa5324ff191b68bfc9582308e4b853136df0
rezatajari <reza.tajari70@gmail.com>
2026-06-23 17:45:57 +0330

    feat(shared): create User entity with data annotations

---
+++ LibraryAppMVC.Shared/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAppMVC.Shared.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Full Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+    public string Email { get; set; } = string.Empty;
+
+    public string? PhoneNumber { get; set; }
+
+    public DateTime MembershipDate { get; set; } = DateTime.Now;
+}
