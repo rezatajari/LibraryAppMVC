840d6481eae37d7e9dceface3fa94d1a94ee7d3a
rezatajari <reza.tajari70@gmail.com>
2026-06-23 15:04:03 +0330

    fix: use official CDN link for Bootstrap Icons to resolve missing woff2 font files

--- LibraryAppMVC.Client/Program.cs
+++ LibraryAppMVC.Client/Program.cs
@@ -6,6 +6,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new Uri("https://localhost:7136"));
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7136") });
 await builder.Build().RunAsync();
